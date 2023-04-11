8ffbdd563dc23a244735f698a372b0173af4f6b0
Ekaterina Osipova <osipovarev@gmail.com>
2023-04-11 17:55:54 -0400

    wrote script to compute consensus from the AA alignment

---
+++ compute_consensus_from_msa.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+#
+import argparse
+from Bio import AlignIO
+from Bio.Align import AlignInfo
+import sys
+
+
+__author__ = "Ekaterina Osipova, 2023."
+
+
+def read_msa(msa):
+	## Load the alignment file in fasta format
+	
+	alignment = AlignIO.read(msa, "fasta")
+	return alignment
+
+
+def compute_consensus(alignment):
+	## Create a summary object and calculate the consensus sequence
+
+	summary = AlignInfo.SummaryInfo(alignment)
+	consensus = summary.dumb_consensus(threshold=0.5)
+	return str(consensus)
+
+
+def mark_according_consensus(alignment, consensus):
+	## Goes through all sequences in the alignment site by site;
+	## Outputs 1/0/- according to consensus match/mismatch/gap 
+
+	for record in alignment:
+		
+		new_record_seq = ''
+
+		for i in range(len(record.seq)):
+			site = record.seq[i]
+			consensus_site = consensus[i]
+
+			if site == '-':
+				new_record_seq += '-'
+			elif site == consensus_site:
+				new_record_seq += '1'
+			else:
+				new_record_seq += '0'
+		print('>' + record.id)
+		print(new_record_seq)
+
+
+def main():
+	## Parse arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-f', '--fasta', type=str, help='MSA in fasta format')
+	args = parser.parse_args()
+
+	## Read alignment in fasta format
+	alignment = read_msa(args.fasta)
+	
+	## Calculte consensus
+	consensus = compute_consensus(alignment)
+
+	## Print the alignment marked according to consensus
+	mark_according_consensus(alignment, consensus)
+	
+
+if __name__ == "__main__":
+	main()
